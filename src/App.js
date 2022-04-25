14302ae3be7e87f08bf662796f525df61eab75fa
Bg8713 <beng8713@gmail.com>
2022-04-25 03:03:54 -0400

    fetch changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
   const [bmi, setBMI] = useState(0);
-  const [calcList, setCalcList] = useState([]);
   const [models, setModels] = useState([]);
 
   async function createCalc(){
@@ -25,12 +24,10 @@ function App() {
 
   async function fetchCalcs(){
     let calcs = await DataStore.query(Calculation);
-    console.log(calcs)
     setModels(calcs);
   }
   
   useEffect(() => {
-    updateList();
     if (bmi != 0){
       createCalc();
     }
@@ -42,59 +39,52 @@ function App() {
     setBMI(703*(weight/(height**2)));
     
   };
- 
-  function updateList(){
-    let newCalc = [weight, height, bmi];
-    setCalcList([...calcList, newCalc ]); 
-  }
 
   return (
-//<Authenticator>
-        <div className="App">
-          <h1 align="left">BMI Calculator</h1>
-          <table>
-            <tbody>
+    <div className="App">
+      <h1 align="left">BMI Calculator</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td align='right'><label id='height'>Height:</label></td>
+            <td align='left'><input type='text' id='height' value={height} onChange={e => setHeight(e.target.value)}/></td>
+          </tr>
+          <tr>
+            <td align='right'><label id="weight">Weight:</label></td>
+            <td align='left'><input type='text' id='weight' value={weight} onChange={e => setWeight(e.target.value)}/></td>
+          </tr>
+          <tr>
+            <td colSpan={2} align='center'><button id='calc' onClick={calc}>Calculate</button></td>
+          </tr>
+          <tr>
+            <td align='right'><label id='bmi'>BMI:</label></td>
+            <td align='left'><input type='text' id='bmi' readOnly value={bmi}/></td>
+          </tr>
+        </tbody>
+      </table>
+      <br/>
+      <table id='calcs'  >
+          <thead>
+            <tr>
+              <th>Weight</th>
+              <th>Height</th>
+              <th>BMI</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {models.map((singleCalc) => 
               <tr>
-                <td align='right'><label id='height'>Height:</label></td>
-                <td align='left'><input type='text' id='height' value={height} onChange={e => setHeight(e.target.value)}/></td>
+                <td key={singleCalc.Weight} >{singleCalc.Weight}</td>
+                <td key={singleCalc.Height} >{singleCalc.Height}</td>
+                <td key={singleCalc.BMI} >{singleCalc.BMI}</td>
+                <td key={singleCalc.createdAt}>{singleCalc.createdAt}</td>
               </tr>
-              <tr>
-                <td align='right'><label id="weight">Weight:</label></td>
-                <td align='left'><input type='text' id='weight' value={weight} onChange={e => setWeight(e.target.value)}/></td>
-              </tr>
-              <tr>
-                <td colSpan={2} align='center'><button id='calc' onClick={calc}>Calculate</button></td>
-              </tr>
-              <tr>
-                <td align='right'><label id='bmi'>BMI:</label></td>
-                <td align='left'><input type='text' id='bmi' readOnly value={bmi}/></td>
-              </tr>
-            </tbody>
-          </table>
-          <br/>
-          <table id='calcs'  >
-              <thead>
-                <tr>
-                  <th>Weight</th>
-                  <th>Height</th>
-                  <th>BMI</th>
-                  <th>Date</th>
-                </tr>
-              </thead>
-              <tbody>
-                {models.map((singleCalc) => 
-                  <tr>
-                    <td key={singleCalc.Weight} >{singleCalc.Weight}</td>
-                    <td key={singleCalc.Height} >{singleCalc.Height}</td>
-                    <td key={singleCalc.BMI} >{singleCalc.BMI}</td>
-                    <td key={singleCalc.createdAt}>{singleCalc.createdAt}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-            <AmplifySignOut/>
-        </div>
-    //</Authenticator>
+            )}
+          </tbody>
+        </table>
+        <AmplifySignOut/>
+    </div>
   );
 }
 
